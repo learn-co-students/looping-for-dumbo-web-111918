dd375e9b15335bfddc55650b22293e7decc700c0
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-30 20:55:08 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,19 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
+for check in checklist
+	puts "Wingardium Leviosa"
+end
 end
 
 
+
+# for counter in 1..40 do
+#   puts "The current number is #{counter}."
+# end
+
+# shoe_count = 1..32
+#
+# for shoe in shoe_count
+#     puts "I'm putting a pair of shoes away."
+# end
